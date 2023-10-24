4ad3bc58e809aa3dbb7143d9db70d9d5f4fe35e4
Simon Lee <simon.lee711@gmail.com>
2023-10-24 17:45:53 +0000

    updated a stratified test train split

--- models/utility.py
+++ models/utility.py
@@ -30,3 +30,24 @@ def train_validate_test_split(df, train_percent=.7, validate_percent=.15, seed=7
     validate = validate.set_index('index')
     test = test.set_index('index')
     return train, validate, test
+
+
+def train_validate_test_split(df, target_column, train_percent=0.7, validate_percent=0.15, seed=None):
+    np.random.seed(seed)
+    df = df.sample(frac=1).reset_index(drop=True)  # shuffle the dataframe
+    targets = df[target_column].unique()
+    train_indices, test_indices = train_test_split_by_stratified_indices(df, target_column, targets, train_percent)
+    validate_percent_of_remaining = validate_percent / (1 - train_percent)
+    validate_indices, test_indices = train_test_split_by_stratified_indices(df.loc[test_indices], target_column, targets, validate_percent_of_remaining)
+    return df.loc[train_indices], df.loc[validate_indices], df.loc[test_indices]
+
+def train_test_split_by_stratified_indices(df, target_column, targets, train_percent):
+    train_indices = []
+    test_indices = []
+    for target in targets:
+        target_indices = df[df[target_column] == target].index.to_list()
+        np.random.shuffle(target_indices)
+        train_size = int(train_percent * len(target_indices))
+        train_indices.extend(target_indices[:train_size])
+        test_indices.extend(target_indices[train_size:])
+    return train_indices, test_indices
